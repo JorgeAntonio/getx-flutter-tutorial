5d9c1d107b1e2b3908ff73f712e961ea239a4ae1
Antonio Del Aguila <56671752+JorgeAntonio@users.noreply.github.com>
2023-05-21 13:28:32 -0500

    Obx

--- lib/src/views/pages/home_page.dart
+++ lib/src/views/pages/home_page.dart
@@ -6,46 +6,53 @@ import 'package:getxtutorial/src/controllers/home_controller.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  /*
+
+    Getx like Obx => Like Streaming
+    - Listen to the changes of the state time to time.
+
+    GetBuilder:
+    - It is used only ONE TIME to rebuild the UI or a rebuild a WIDGET when the state changes.
+  
+  */ 
+
   @override
   Widget build(BuildContext context) {
+    HomeController controller = Get.put(HomeController());
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
         centerTitle: true,
         actions: [
           TextButton(
-            onPressed: () {
-              Get.toNamed('/firstpage');
-            },
-            child: const Text('Next')
-          ),
+              onPressed: () {
+                Get.toNamed('/firstpage');
+              },
+              child: const Text('Next')),
         ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GetX<HomeController>(
-              init: HomeController(),
-              builder: (controller) {
-                return Column(
-                  children: [
-                    Text(
-                      controller.index.value.toString(),
-                      style: const TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        controller.increment();
-                      },
-                      child: const Text('Increment'),
-                    ),
-                  ],
-                );
-              },
+            Obx(
+              () => Column(
+                children: [
+                  Text(
+                    controller.index.value.toString(),
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.increment();
+                    },
+                    child: const Text('Increment'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
