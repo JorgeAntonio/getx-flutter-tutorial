b499f9b4af6bbaa3c2afc3dce139dfad45d3dcbe
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-05-24 17:48:22 -0500

    Get Connect v3

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -4,7 +4,7 @@ import 'package:getxtutorial/src/models/news_model.dart';
 class NewsService extends GetConnect implements GetxService {
   Future<NewsApi> getNewsData() async {
     var response = await get(
-        'https://newsapi.org/v2/everything?q=tesla&from=2023-04-24&sortBy=publishedAt&apiKey=139b5cec42724cb1ba7c56f9f28b3bea');
+        'https://newsapi.org/v2/everything?q=tesla&from=2023-04-24&sortBy=publishedAt&apiKey=API_KEY');
 
     return NewsApi.fromJson(response.body);
   }
